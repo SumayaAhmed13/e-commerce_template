866150cc31e94fe071f7427afb0d2bb6509a5037
SumayaShemo <sumayaahmedbd@gmail.com>
2024-01-02 21:13:56 +0600

    search page update

--- src/Pages/SearchPage.jsx
+++ src/Pages/SearchPage.jsx
@@ -1,80 +1,47 @@
 import React from 'react'
-
+import { NavLink } from "react-router-dom";
 
  const SearchPage = () => {
   return (
-    <div className='cart-wrap'>
- 	    <div className="container">
-	        <div className="row">
-			    <div className="col-md-12">
-			        <div className="main-heading mb-10">Search Result</div>
-			        <div className="table-wishlist">
-				        <table cellPadding="0" cellSpacing="0" border="0" width="100%">
-				        	<thead>
-					        	<tr>
-					        		<th width="45%">Product Name</th>
-					        		<th width="15%">Unit Price</th>
-					        		<th width="15%">Stock Status</th>
-					        		<th width="15%"></th>
-					        		<th width="10%"></th>
-					        	</tr>
-					        </thead>
-					        <tbody>
-					        	<tr>
-					        		<td width="45%">
-					        			<div className="display-flex align-center">
-		                                    <div className="img-product ">
-		                                        <img src="https://www.91-img.com/pictures/laptops/asus/asus-x552cl-sx019d-core-i3-3rd-gen-4-gb-500-gb-dos-1-gb-61721-large-1.jpg" alt="" />
-		                                    </div>
-		                                    <div className="name-product">
-		                                        Apple iPad Mini
-		                                    </div>
-	                                    </div>
-	                                </td>
-					        		<td width="15%" className="price">$110.00</td>
-					        		<td width="15%"><span className="in-stock-box">In Stock</span></td>
-					        		<td width="15%"><button className="round-black-btn small-btn">Add to Cart</button></td>
-					        		<td width="10%" className="text-center"><a href="#" className="trash-icon"><i className="bi bi-trash"></i></a></td>
-					        	</tr>
-					        	<tr>
-					        		<td width="45%">
-					        			<div className="display-flex align-center">
-		                                    <div className="img-product">
-		                                        <img src="https://www.91-img.com/pictures/laptops/asus/asus-x552cl-sx019d-core-i3-3rd-gen-4-gb-500-gb-dos-1-gb-61721-large-1.jpg" alt="" />
-		                                    </div>
-		                                    <div className="name-product">
-		                                        Apple iPad Mini
-		                                    </div>
-	                                    </div>
-	                                </td>
-					        		<td width="15%" className="price">$110.00</td>
-					        		<td width="15%"><span className="in-stock-box">In Stock</span></td>
-					        		<td width="15%"><button className="round-black-btn small-btn">Add to Cart</button></td>
-					        		<td width="10%" className="text-center"><a href="#" className="trash-icon"><i className="bi bi-trash"></i></a></td>
-					        	</tr>
-					        	<tr>
-					        		<td width="45%">
-					        			<div className="display-flex align-center">
-		                                    <div className="img-product">
-		                                        <img src="https://www.91-img.com/pictures/laptops/asus/asus-x552cl-sx019d-core-i3-3rd-gen-4-gb-500-gb-dos-1-gb-61721-large-1.jpg" alt="" />
-		                                    </div>
-		                                    <div className="name-product">
-		                                        Apple iPad Mini
-		                                    </div>
-	                                    </div>
-	                                </td>
-					        		<td width="15%" className="price">$110.00</td>
-					        		<td width="15%"><span className="in-stock-box">In Stock</span></td>
-					        		<td width="15%"><button className="round-black-btn small-btn">Add to Cart</button></td>
-					        		<td width="10%" className="text-center"><a href="#" className="trash-icon"><i className="bi bi-trash"></i></a></td>
-					        	</tr>
-				        	</tbody>
-				        </table>
-				    </div>
-			    </div>
-			</div>
-		</div>
-    </div>
+	<div className="container mt-5 border">
+    <h3>Search Result</h3>
+    <table className="table mt-5">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">Price</th>
+            <th scope="col">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Dell Laptop</td>
+            <td>1</td>
+            <td>90000</td>
+            <td>
+                <a className="btn btn-success rounded-pill">Stock In</a>
+				<NavLink to="/cartPage" className="btn btn-outline-secondary rounded-pill mx-3"> Add To cart</NavLink>
+                <button className="btn btn-outline-danger rounded-pill me-3">Remove</button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Apple MacBook Pro</td>
+            <td>1</td>
+            <td>150000</td>
+            <td>
+                <a className="btn btn-success rounded-pill">Stock In</a>
+				<NavLink to="/cartPage" className="btn btn-outline-secondary rounded-pill mx-3"> Add To cart</NavLink>
+				<button className="btn btn-outline-danger rounded-pill me-3">Remove</button>
+            </td>
+          </tr>
+          
+        </tbody>
+      </table>
+  </div>
  
 
 
